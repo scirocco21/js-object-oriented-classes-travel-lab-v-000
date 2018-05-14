846a4dbd2bdc2b9415e477775b07ceb2f367ee7e
Seb Gertz <sebgertz@gmail.com>
2018-05-14 19:35:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf(year) {
+    return parseInt(year) - this.startDate;
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+}
