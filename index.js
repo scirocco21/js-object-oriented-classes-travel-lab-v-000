d8cab79f1af93f89fecc9f13a4eb4491bf59cdca
Seb Gertz <sebgertz@gmail.com>
2018-05-14 21:16:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,29 @@ class Route {
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
   }
+  let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+  blocksTravelled() {
+    let startingVertical = eastWest.indexOf(beginningLocation.vertical);
+    let endingVertical = eastWest.indexOf(endingLocation.vertical);
+    let verticalBlocks;
+    if (startingVertical > endingVertical) {  
+      verticalBlocks = startingVertical - endingVertical;
+    else if (startingVertical < endingVertical) {
+      verticalBlocks = endingVertical - startingVertical;
+    else {
+      verticalBlocks = 0;
+    }
+  let startingHorizontal = eastWest.indexOf(beginningLocation.horizontal); 
+  let endingHorizontal = eastWest.indexOf(endingLocation.horizontal);
+  let horizontalBlocks;
+
+  if (startingHorizontal > endingHorizontal) {
+    horizontalBlocks = startingHorizontal - endingHorizontal;
+  } else if (endingHorizontal > startingHorizontal) {
+      horizontalBlocks = endingHorizontal - startingHorizontal;
+  } else {
+    horizontalBlocks = 0;
+  }
+  return horizontalBlocks + verticalBlocks;
 }
